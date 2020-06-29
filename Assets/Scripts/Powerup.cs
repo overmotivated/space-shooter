c157908fac2a08a363452e163d9e79d32bbf40a2
Nat <cupoffappuccino@gmail.com>
2020-06-30 01:47:51 +0300

    fix: powerups destoy

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -35,6 +35,7 @@ public class Powerup : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             PowerupManager.UsePowerup(powerupEnum);
+            Destroy(gameObject);
         }
     }
 }
